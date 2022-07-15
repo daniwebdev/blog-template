657a5daa4735781425eefedd3d84ff0e55f6aa2b
Dani <38590276+daniwebdev@users.noreply.github.com>
2022-07-15 15:35:18 +0700

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const fileinclude = require("gulp-file-include");
+
+gulp.task("default", () => {
+  return gulp
+    .src(["src/*.html", "src/**/*.js"])
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.watch("src/**/*.{html,js,css}", gulp.series("default"));
---
+++ src/app.js
@@ -0,0 +1,62 @@
+const NAVBAR = document.querySelector('nav');
+
+/* ---------------------------------- */
+/* BEGIN DARKMODE TOGGLE */
+/* ---------------------------------- */
+var darkmodeToggle = document.querySelector('#darkmode-toggle');
+var darkmodeBall = document.querySelector('#darkmode-toggle .ball');
+var darkmodeState = localStorage.getItem('darkmode');
+
+if(darkmodeState === 'true') {
+    document.documentElement.classList.remove('light');
+    document.documentElement.classList.add('dark');
+
+    darkmodeBall.style.left = '35px';
+} else {
+    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.add('light');
+
+    darkmodeBall.style.left = '1px';
+}
+
+darkmodeToggle.addEventListener('click', () => {
+
+    /* if darkmode is enabled */
+    if(darkmodeBall.style.left == '35px') {
+        darkmodeBall.style.left = '1px';
+        darkmodeBall.style.transition = ".2s linear";
+
+        document.documentElement.classList.toggle('dark')
+        document.documentElement.classList.toggle('light')
+
+        localStorage.setItem('darkmode', 'false');
+    } else {
+        /* if darkmode is disabled */
+        darkmodeBall.style.left = '35px';
+        darkmodeBall.style.transition = ".2s linear";
+
+        document.documentElement.classList.toggle('dark')
+        document.documentElement.classList.toggle('light')
+
+        localStorage.setItem('darkmode', 'true');
+    }
+
+
+    console.log(darkmodeBall.style.left)
+})
+
+/* ---------------------------------- */
+/* END DARKMODE TOGGLE */
+/* ---------------------------------- */
+
+
+
+
+/* ---------------------------------- */
+/* TOGGLE MENU MOBILE */
+/* ---------------------------------- */
+function toggleMenu() {
+    const fullMenu = document.querySelector('.full-menu');
+
+    fullMenu.classList.toggle('show');
+}
